78bf9c097fe4f79b66d2561d7c787a719bf8098c
skdGT <skidru00@gmail.com>
2019-12-25 20:39:57 +0000

    first commit part2

---
+++ parte2/classes/Servico.cpp
@@ -0,0 +1,23 @@
+//
+// Created by skidr on 25/12/2019.
+//
+
+#include "Servico.h"
+
+Servico::Servico(float custo, string prestador, string servico) {
+    this->custo = custo;
+    this->prestador = prestador;
+    this->servico = servico;
+}
+
+string Servico::getTipo() {
+    return servico;
+}
+
+float Servico::getCusto() {
+    return custo;
+}
+
+string Servico::getPrestador() {
+    return prestador;
+}
---
+++ parte2/classes/Servico.h
@@ -0,0 +1,57 @@
+//
+// Created by skidr on 25/12/2019.
+//
+
+#ifndef PARTE2_SERVICO_H
+#define PARTE2_SERVICO_H
+
+/**
+ * @file Servico.h
+ * @brief Classe e Métodos para Serviço
+ */
+
+#include <string>
+
+using namespace std;
+
+/**
+ * @brief Classe Serviço
+ *
+ * Métodos GET definidos
+ */
+class Servico {
+    float custo;        //!< Custo do serviço prestado
+    string prestador;   //!< Nome da empresa ou do functionário prestador do serviço
+    string servico;     //!< Serviço prestado
+public:
+
+    /** @brief Construtor da Classe Serviço
+     *
+     * @param custo         Custo do serviço prestado
+     * @param prestador     Nome da empresa ou do functionário prestador do serviço
+     * @param servico       Serviço prestado
+     */
+    Servico(const float custo, string prestador, string servico);
+
+    /**
+     *
+     * @return Tipo do serviço prestado (limpeza, cabeleireiro, jardineiro, etc.)
+     */
+    string getTipo();
+
+    /**
+     *
+     * @return Custo do serviço prestado
+     */
+    float getCusto();
+
+    /**
+     *
+     * @return Prestador do serviço
+     */
+    string getPrestador();
+};
+
+
+
+#endif //PARTE2_SERVICO_H
