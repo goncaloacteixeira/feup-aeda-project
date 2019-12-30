118873d6bacdfd2befcb7b6179de87e113bac8cd
skdGT <skidru00@gmail.com>
2019-12-30 12:03:21 +0000

    menus & output-formats documented

--- parte2/output-formats.h
+++ parte2/output-formats.h
@@ -8,6 +8,18 @@
 #include "classes/CAgency.h"
 #include "fort.hpp"
 
+/**
+ * @file output-formats.h
+ * @brief Funçoes para aprensentar info em forma de tabelas
+ *
+ * - Vetor de habitaçoes\n
+ * - Vetor de condóminos\n
+ * - HashTab de antigos condóminos\n
+ * - Vetor de antigos condóminos\n
+ * - Vetor de transportes (paragens)\n
+ * - BST de condominios\n
+ * - Vetor de Serviços
+ */
 
 string printTable(vector<Habitacao*> habitacoes);
 string printTable(vector<Condomino*> condominos);
